53cfdd94f81d13e1553ffef22d4fbc510a88dd15
Krylusov <164981066+Krylusov@users.noreply.github.com>
2026-02-22 03:30:00 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'master-calc-v7';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
-];
+const CACHE_NAME = 'master-calc-v7.1';
+const ASSETS = ['index.html', 'manifest.json', 'icon.png'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
